ed1159311994c148546af3e81800e65022333824
LAPTOP-LHUOQ7VL\91700 <ranjan.intel@gmail.com>
2022-05-10 13:57:21 +0530

    project created

---
+++ src/Account.java
@@ -0,0 +1,40 @@
+public class Account {
+    private int number;
+    private int balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+    
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public int getBalance() {
+        return balance;
+    }
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    
+}
